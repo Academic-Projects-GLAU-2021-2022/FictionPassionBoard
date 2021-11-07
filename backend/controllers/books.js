ed8f79ce358ca8be095c6a52f0d3579399a8ddfd
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-08 00:47:22 +0530

    adding method for getting books by their id

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -10,3 +10,17 @@ let DUMMY_BOOKS = [
     description: "A book for learning C Programming Concepts",
   },
 ];
+
+const getBookById = (req, res, next) => {
+  const bookId = req.params.bid; // for book id {bid refers to b1}
+
+  const book = DUMMY_BOOKS.find((b) => {
+    return b.id === bookId;
+  });
+
+  if (!book) {
+    throw new HttpError("Could not find a book for the provided id.", 404);
+  }
+
+  res.json({ book }); //it refers to the complete book object
+};
