dbc5af077afaac84f7594cfe9fb229ba50b91a76
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-25 21:14:29 +0530

    adding index.js file

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import FictionPassionBoard from "./FictionPassionBoard";
+
+ReactDOM.render(<FictionPassionBoard />, document.getElementById("root"));
