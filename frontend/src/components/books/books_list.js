16212da4007a7da56cff9f2926d11ead989a020c
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-27 04:30:07 +0530

    adding dummy book array

---
+++ frontend/src/components/books/books_list.js
@@ -0,0 +1,75 @@
+export const booklists = [
+  {
+    id: "1",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51dTESzz0DL._SX336_BO1,204,203,200_.jpg",
+    title: "A Commentary & Digest on The Air,Act 1981",
+    author: "APOORVA K SINGH, I.A.S",
+  },
+  {
+    id: "2",
+    image: "https://pbs.twimg.com/media/EKiJSiwWkAAAaf1.jpg",
+    title: "Bloomsbury Anthology of Great Indian Poems",
+    author: "ABHAY K.",
+  },
+  {
+    id: "3",
+    image: "https://images-na.ssl-images-amazon.com/images/I/41R7gJgF3mL.jpg",
+    title: "Design And Analysis Algorithm",
+    author: "S.Sridhar",
+  },
+  {
+    id: "4",
+    image: "https://m.media-amazon.com/images/I/41QGW4dujhL._SL500_.jpg",
+    title: "Intoduction to Data Science",
+    author: "RAFAEL A. IRIZARRY",
+  },
+  {
+    id: "5",
+    image: "https://images-na.ssl-images-amazon.com/images/I/81dabeF8m8L.jpg",
+    title: "COMPUTER NETWORKS",
+    author: "ANDREW S. TANENBAUM",
+  },
+  {
+    id: "6",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51dTESzz0DL._SX336_BO1,204,203,200_.jpg",
+    title: "A Commentary & Digest on The Air,Act 1981",
+    author: "APOORVA K SINGH, I.A.S",
+  },
+  {
+    id: "7",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51dTESzz0DL._SX336_BO1,204,203,200_.jpg",
+    title: "A Commentary & Digest on The Air,Act 1981",
+    author: "APOORVA K SINGH, I.A.S",
+  },
+  {
+    id: "8",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51dTESzz0DL._SX336_BO1,204,203,200_.jpg",
+    title: "A Commentary & Digest on The Air,Act 1981",
+    author: "APOORVA K SINGH, I.A.S",
+  },
+  {
+    id: "9",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51dTESzz0DL._SX336_BO1,204,203,200_.jpg",
+    title: "A Commentary & Digest on The Air,Act 1981",
+    author: "APOORVA K SINGH, I.A.S",
+  },
+  {
+    id: "10",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51dTESzz0DL._SX336_BO1,204,203,200_.jpg",
+    title: "A Commentary & Digest on The Air,Act 1981",
+    author: "APOORVA K SINGH, I.A.S",
+  },
+  {
+    id: "11",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51dTESzz0DL._SX336_BO1,204,203,200_.jpg",
+    title: "A Commentary & Digest on The Air,Act 1981",
+    author: "APOORVA K SINGH, I.A.S",
+  },
+];
